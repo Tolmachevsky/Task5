f0b456ca09e54d55b6b293f65e25de383e083a59
Tolmachevsky <Tolmachevsky001@ya.ru>
2022-10-21 05:42:03 +0300

    AdminController removed

--- Task5.1/src/main/java/ru/tolmachevsky/tasks/controller/UserController.java
+++ Task5.1/src/main/java/ru/tolmachevsky/tasks/controller/UserController.java
@@ -3,10 +3,12 @@ package ru.tolmachevsky.tasks.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.tolmachevsky.tasks.model.User;
 import ru.tolmachevsky.tasks.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @Controller
@@ -21,4 +23,36 @@ public class UserController {
         return "user_table";
     }
 
+    @GetMapping(path = "/admin")
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "users_table";
+    }
+
+    @GetMapping(path = "/admin/update/{id}")
+    public String updateUserForm(@PathVariable("id") Integer id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "update_user";
+    }
+
+    @PostMapping(path = "/admin/update")
+    public String updateUser(User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping(path = "/admin/delete/{id}")
+    public String deleteUserById(@PathVariable("id") Integer id) {
+        userService.deleteUserById(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping(path = "/admin/makeAdmin/{id}")
+    public String makeAdmin(@PathVariable("id") Integer id) {
+        userService.findById(id).makeAdmin();
+        User user = userService.findById(id);
+        userService.save(user);
+        return "redirect:/admin";
+    }
 }
